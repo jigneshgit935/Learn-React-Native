17cbb84c428f32c5c4ad7e17ea8011834a851fa5
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-18 22:52:37 +0530

    Image and Background/Image Component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { Text } from 'react-native';
+import { Image, ImageBackground, Text } from 'react-native';
 import { View } from 'react-native';
-
+import adaptive from './assets/adaptive-icon.png';
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
@@ -8,6 +8,15 @@ export default function App() {
         <Text style={{ color: 'white' }}>Hello</Text>
         &nbsp; React Native
       </Text>
+      {/* <Image source={adaptive} style={{ width: 300, height: 300 }} /> */}
+      {/* <Image
+        source={{ uri: 'https://picsum.photos/300' }}
+        style={{ width: 300, height: 300 }}
+      /> */}
+
+      <ImageBackground style={{ flex: 1 }} source={adaptive}>
+        <Text>Image text</Text>
+      </ImageBackground>
     </View>
   );
 }

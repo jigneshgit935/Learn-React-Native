cde627b3bae771c327124e14b638f9dc64fac05c
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-26 22:22:17 +0530

    modal-component

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
-import { Button, Image, Pressable, ScrollView, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  Modal,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import Logo from '../reactNative/assets/adaptive-icon.png';
+import { useState } from 'react';
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
       <Button
         title="Press"
-        onPress={() => console.log('Button Press')}
+        onPress={() => setIsModalVisible(true)}
         color="midnightblue"
       />
-      <Pressable onPress={() => console.log('Image pressed')}>
-        <Image source={Logo} style={{ width: 300, height: 300 }} />
-      </Pressable>
-      <Pressable
-        onLongPress={() => console.log('Text pressed')}
-        onPressOut={() => console.log('Long press out')}
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        // animationType="fade"
+        presentationStyle="pageSheet"
       >
-        <Text>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-          section 1.10.32. The standard chunk of Lorem Ipsum used since the
-          1500s is reproduced below for those interested. Sections 1.10.32 and
-          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
-        </Text>
-      </Pressable>
+        <View style={{ flex: 1, backgroundColor: 'lightblue', padding: 60 }}>
+          <Text>Modal Content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }

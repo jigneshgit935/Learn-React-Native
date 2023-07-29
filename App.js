7b8fadf1d16669dc74ce4beaa66063f734e9356a
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-29 10:42:24 +0530

    Custom component

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import { ActivityIndicator, Alert, Button, View } from 'react-native';
+import Greet from './components/Greet';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Button title="Alert" onPress={() => Alert.alert('Invalid Data')} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert('Invalid Data', 'DOB Incorrect')}
-      />
-
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert('Invalid Data', 'DOB Incorrect', [
-            {
-              text: 'Cancel',
-              onPress: () => console.log('Cancel Pressed'),
-            },
-            {
-              text: 'Ok',
-              onPress: () => console.log('Ok Pressed'),
-            },
-          ])
-        }
-      />
+      <Greet name="Jignesh Sharma" />
+      <Greet name="Rajesh Audichya" />
+      <Greet name="Sunil Chetri" />
+      <Greet name="Manish bhai" />
+      <Greet name="tony Stark" />
     </View>
   );
 }

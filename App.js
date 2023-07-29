ce187729371c3650d2bc3c787ea92f38fcebbcd6
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-29 10:36:32 +0530

    Alert Component

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, Alert, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <ActivityIndicator />
-      <ActivityIndicator size={50} />
-      <ActivityIndicator size={50} color="red" />
-      <ActivityIndicator size={50} color="blue" animating={false} />
+      <Button title="Alert" onPress={() => Alert.alert('Invalid Data')} />
+      <Button
+        title="Alert 2"
+        onPress={() => Alert.alert('Invalid Data', 'DOB Incorrect')}
+      />
+
+      <Button
+        title="Alert 3"
+        onPress={() =>
+          Alert.alert('Invalid Data', 'DOB Incorrect', [
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+            },
+            {
+              text: 'Ok',
+              onPress: () => console.log('Ok Pressed'),
+            },
+          ])
+        }
+      />
     </View>
   );
 }

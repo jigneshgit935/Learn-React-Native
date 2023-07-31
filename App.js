d5076c0770ca967251a743c00494b18c0e92d214
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-31 15:57:33 +0530

    styleSheet Api

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { ActivityIndicator, Alert, Button, View } from 'react-native';
-import Greet from './components/Greet';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Greet name="Jignesh Sharma" />
-      <Greet name="Rajesh Audichya" />
-      <Greet name="Sunil Chetri" />
-      <Greet name="Manish bhai" />
-      <Greet name="tony Stark" />
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    padding: 60,
+  },
+  title: { fontSize: 20, fontWeight: 600 },
+});

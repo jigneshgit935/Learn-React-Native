305ac394f61462f6cac321ddd53022c5790be04f
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 04:52:01 +0530

    flex-direction

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: '#8e9b00', flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: '#b65d1f', flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
-      <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      {/* <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
       <Box style={{ backgroundColor: '#6b0803' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
-      <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
+      <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box> */}
     </View>
   );
 }
@@ -22,5 +22,9 @@ const styles = StyleSheet.create({
     borderWidth: 6,
     borderColor: 'red',
     flex: 1,
+    // flexDirection: 'column-reverse',
+    // flexDirection: 'column',
+    // flexDirection: 'row',
+    flexDirection: 'row-reverse',
   },
 });

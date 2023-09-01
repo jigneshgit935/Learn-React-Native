b500259e8c3d312d5ac5be2e196a1b1cc632a297
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 04:47:51 +0530

    commited flex-basics

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import Box from './components/Box';
 
 export default function App() {
   return (
+    // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
-      <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#8e9b00', flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#b65d1f', flex: 3 }}>Box 2</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
       <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
       <Box style={{ backgroundColor: '#6b0803' }}>Box 5</Box>
@@ -16,5 +17,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { marginTop: 64, borderWidth: 6, borderColor: 'red' },
+  container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: 'red',
+    flex: 1,
+  },
 });

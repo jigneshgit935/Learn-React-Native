ab6f991e662665cd1f9f2e9e7d62a7ca2ef62604
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 16:47:38 +0530

    flex-basis

--- App.js
+++ App.js
@@ -7,8 +7,12 @@ export default function App() {
     <View style={styles.container}>
       <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
       <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
-      <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
-      <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      <Box style={{ backgroundColor: '#1c4c56', flexBasis: 140, flex: 1 }}>
+        Box 3
+      </Box>
+      <Box style={{ backgroundColor: '#ab9156', height: 140, flex: 1 }}>
+        Box 4
+      </Box>
       <Box style={{ backgroundColor: '#6b0803' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
       <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
@@ -18,13 +22,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    height: 360,
-    flexWrap: 'wrap',
-    // rowGap: 20,
-    // columnGap: 20,
-    gap: 22,
-    alignContent: 'center',
+    flex: 1,
+
     marginTop: 64,
     borderWidth: 6,
     borderColor: 'red',

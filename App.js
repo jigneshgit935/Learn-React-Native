7106fe2ad72e28822f0568919cbc553df43f6e78
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-05 19:50:16 +0530

    completed pokemon card design exercise

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StyleSheet, View } from 'react-native';
 import PokemonCard from './Compoents/PokemonCard';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
 
 const App = () => {
+  const charmanderData = {
+    name: 'Charmander',
+    image: require('./assets/charmander.png'),
+    type: 'Fire',
+    hp: 39,
+    moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
+    weaknesses: ['Water', 'Rock'],
+  };
+
+  const squirtleData = {
+    name: 'Squirtle',
+    image: require('./assets/squirtle.png'),
+    type: 'Water',
+    hp: 44,
+    moves: ['Tackle', 'Water Gun', 'Tail Whip', 'Withdraw'],
+    weaknesses: ['Electric', 'Grass'],
+  };
+
+  const bulbasaurData = {
+    name: 'Bulbasaur',
+    image: require('./assets/bulbasaur.png'),
+    type: 'Grass',
+    hp: 45,
+    moves: ['Tackle', 'Vine Whip', 'Growl', 'Leech Seed'],
+    weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
+  };
+
+  const pikachuData = {
+    name: 'Pikachu',
+    image: require('./assets/pikachu.png'),
+    type: 'Electric',
+    hp: 35,
+    moves: ['Quick Attack', 'Thunderbolt', 'Tail Whip', 'Growl'],
+    weaknesses: ['Ground'],
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <PokemonCard />
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- Compoents/PokemonCard.js
+++ Compoents/PokemonCard.js
@@ -1,10 +1,50 @@
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, Image } from 'react-native';
 import React from 'react';
 
-const PokemonCard = () => {
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case 'electric':
+      return { borderColor: '#FFD700', emoji: '⚡️' };
+    case 'water':
+      return { borderColor: '#6493EA', emoji: '💧' };
+    case 'fire':
+      return { borderColor: '#FF5733', emoji: '🔥' };
+    case 'grass':
+      return { borderColor: '#66CC66', emoji: '🌿' };
+    default:
+      return { borderColor: '#A0A0A0', emoji: '❓' };
+  }
+};
+
+const PokemonCard = ({ name, image, type, moves, hp, weaknesses }) => {
+  const { borderColor, emoji } = getTypeDetails(type);
   return (
     <View style={styles.card}>
-      <Text>PokemonCard</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️{hp}</Text>
+      </View>
+      <Image
+        style={styles.image}
+        source={image}
+        accessibilityLabel={`${name} pokemon`}
+        resizeMode="contain"
+      />
+
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
+      </View>
+
+      <View style={styles.moveContainer}>
+        <Text style={styles.moveText}>Moves: {moves.join(', ')}</Text>
+      </View>
+
+      <View style={styles.weakContainer}>
+        <Text style={styles.weakText}>Weakness: {weaknesses.join(', ')}</Text>
+      </View>
     </View>
   );
 };
@@ -29,5 +69,57 @@ const styles = StyleSheet.create({
         elevation: 5,
       },
     }),
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  badge: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  moveContainer: {
+    marginBottom: 16,
+  },
+  moveText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  weakContainer: {
+    marginBottom: 8,
+  },
+  weakText: {
+    fontSize: 22,
+    fontWeight: 'bold',
   },
 });

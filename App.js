f62b0e25373ad09cfef0ea190e263daba5d414a2
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-05 19:23:08 +0530

    working on pokemon card

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import PokemonCard from './Compoents/PokemonCard';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
-      </View>
+      <PokemonCard />
     </SafeAreaView>
   );
 };
@@ -16,14 +15,6 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'plum',
-  },
-  box: {
-    padding: 20,
-  },
-  text: {
-    fontSize: 24,
-    textAlign: 'center',
-    fontWeight: 'bold',
+    backgroundColor: '#f5f5f5',
   },
 });
---
+++ Compoents/PokemonCard.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import React from 'react';
+
+const PokemonCard = () => {
+  return (
+    <View style={styles.card}>
+      <Text>PokemonCard</Text>
+    </View>
+  );
+};
+
+export default PokemonCard;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});

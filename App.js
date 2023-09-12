b7489c29a3cdd951a62a0618ecbf37f6c72461c3
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-12 18:15:02 +0530

    comir

--- App.js
+++ App.js
@@ -4,15 +4,25 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
         style={styles.input}
         value={name}
         onChangeText={setName}
+        autoCapitalize="words"
         placeholder="Enter your name"
       />
       <Text style={styles.text}>My name is {name} </Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Enter your password"
+        // keyboardType="decimal-pad"
+        secureTextEntry={true}
+      />
     </SafeAreaView>
   );
 };
